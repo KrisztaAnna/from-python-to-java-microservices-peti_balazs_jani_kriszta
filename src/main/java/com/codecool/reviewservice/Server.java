65b2b34512ee8e2512c1a53c47ecf435af6907fc
KrisztinaAnna <krisztina.baranyai@gmail.com>
2017-01-04 14:09:31 +0100

    add routes to registration page and for new registration

--- src/main/java/com/codecool/reviewservice/Server.java
+++ src/main/java/com/codecool/reviewservice/Server.java
@@ -1,8 +1,9 @@
 package com.codecool.reviewservice;
 
-//import com.codecool.reviewservice.controller.Controller;
+import com.codecool.reviewservice.controller.RegistrationPageController;
 import com.codecool.reviewservice.controller.ReviewController;
 import com.codecool.reviewservice.dao.connection.DBConnection;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 import java.sql.SQLException;
 
@@ -10,10 +11,14 @@ import static spark.Spark.*;
 
 public class Server {
 
+
     public static void main(String[] args) throws SQLException {
         // connection to PostgreSQL Database
         DBConnection dbConnection = new DBConnection();
         dbConnection.connect();
+
+        // Instantiate template engine
+        ThymeleafTemplateEngine tmp = new ThymeleafTemplateEngine();
 
         // Default server settings
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
@@ -21,9 +26,11 @@ public class Server {
         port(8888);
 
         // Routes
+//        get("newClient/:name/:email", (request, response) -> ClientController.newClient(request, response));
         get("/review/:APIKey/:productName/:comment/:ratings", (request, response) -> ReviewController.createReview(request, response));
         get("/changeStatus/:APIKey/:reviewKey/:status", (request, response) -> ReviewController.changeStatus(request, response));
         get("reviewFromClient/:APIKey", (request, response) -> ReviewController.getAllReviewFromClient(request, response));
         get("allReviewOfProduct/:APIKey/:ProductName", (request, response) -> ReviewController.getAllReviewOfProduct(request, response));
+        get("/", RegistrationPageController::renderRegistrationPage, tmp);
     }
 }
