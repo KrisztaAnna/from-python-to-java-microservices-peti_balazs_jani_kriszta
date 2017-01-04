e3bbe2bbd0ff6d0e743e86c5422cf4dfeb2962fe
KrisztinaAnna <krisztina.baranyai@gmail.com>
2017-01-04 01:02:14 +0100

    add error for handling invalid client key

---
+++ src/main/java/com/codecool/reviewservice/clienterror/InvalidClient.java
@@ -0,0 +1,13 @@
+package com.codecool.reviewservice.clienterror;
+
+import java.sql.SQLException;
+
+/**
+ * Created by krisztinabaranyai on 04/01/2017.
+ */
+public class InvalidClient extends SQLException {
+
+    public InvalidClient(String s){
+        super(s);
+    }
+}
