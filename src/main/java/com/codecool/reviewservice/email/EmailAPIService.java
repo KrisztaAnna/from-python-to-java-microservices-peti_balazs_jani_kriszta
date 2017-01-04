56d674806a654b7fbd67387191970ae2086b436c
KrisztinaAnna <krisztina.baranyai@gmail.com>
2017-01-04 10:56:03 +0100

    implement sendReviewForModerating method

--- src/main/java/com/codecool/reviewservice/email/EmailAPIService.java
+++ src/main/java/com/codecool/reviewservice/email/EmailAPIService.java
@@ -1,9 +1,12 @@
 package com.codecool.reviewservice.email;
 
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
+import spark.utils.StringUtils;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by krisztinabaranyai on 03/01/2017.
@@ -13,7 +16,16 @@ public class EmailAPIService {
     private static EmailAPIService INSTANCE;
 
 
-    private String execute(URI uri) throws IOException {
+    public static String sendReviewForModerating(String email) throws URISyntaxException, IOException{
+        URIBuilder builder = new URIBuilder(EMAIL_API_URL);
+
+        if (!StringUtils.isEmpty(email)) {
+            builder.addParameter("email", email);
+        }
+        return execute(builder.build());
+    }
+
+    private static String execute(URI uri) throws IOException {
         return Request.Get(uri)
                 .execute()
                 .returnContent()
