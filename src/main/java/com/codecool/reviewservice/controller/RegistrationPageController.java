7e1fb8c3aec1b20e41c8203bcd6218669f6fbb6f
KrisztinaAnna <krisztina.baranyai@gmail.com>
2017-01-10 20:47:54 +0100

    add doc to the RegistrationPageController

--- src/main/java/com/codecool/reviewservice/controller/RegistrationPageController.java
+++ src/main/java/com/codecool/reviewservice/controller/RegistrationPageController.java
@@ -7,8 +7,19 @@ import spark.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * This class is for rendering the registration page
+ *
+ * @author Kriszta
+ */
 public class RegistrationPageController {
+    /**
+     * RegistrationPageController is used to render the registration page of the application for clients.
+     * The route "/" calls this method.
+     * @param request
+     * @param response
+     * @return ModalAndView This returns a ModalAndView object which renders registration.html
+     */
     public static ModelAndView renderRegistrationPage(Request request, Response response) {
         Map params = new HashMap<>();
         return new ModelAndView(params, "registration");
